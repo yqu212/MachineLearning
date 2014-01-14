92763f2db339fad1156fe5e5f7cea84f3f7870b9
yqu212 <yqu212@gmail.com>
2014-01-14 16:16:53 +0800

    Add some comments.

--- gmm.m
+++ gmm.m
@@ -22,9 +22,12 @@ function varargout = gmm(X, K_or_centroids)
 % ============================================================
  
     threshold = 1e-15;
+	% N is the number of samples.
+	% D is the dimension of data.
     [N, D] = size(X);
  
-    if isscalar(K_or_centroids)
+    if isscalar(K_or_centroids) 
+		% K is the number of components 
         K = K_or_centroids;
         % randomly pick centroids
         rndp = randperm(N);
@@ -34,24 +37,30 @@ function varargout = gmm(X, K_or_centroids)
         centroids = K_or_centroids;
     end
  
-    % initial values
+    % initialize mu, sigma and pi.
     [pMiu pPi pSigma] = init_params();
  
     Lprev = -inf;
     while true
+		% E-step
+
+		% Compute the probablity of normal distribution.
         Px = calc_prob();
  
+		% evaluate posterior probability of pi
         % new value for pGamma
         pGamma = Px .* repmat(pPi, N, 1);
         pGamma = pGamma ./ repmat(sum(pGamma, 2), 1, K);
  
-        % new value for parameters of each Component
-        Nk = sum(pGamma, 1);
-        pMiu = diag(1./Nk) * pGamma' * X;
-        pPi = Nk/N;
+		% M-setp
+
+		% reestimate mu, sigma and pi
+        Nk = sum(pGamma, 1); % N_k
+        pMiu = diag(1./Nk) * pGamma' * X; % Mu
+        pPi = Nk/N; % pi
         for kk = 1:K
             Xshift = X-repmat(pMiu(kk, :), N, 1);
-            pSigma(:, :, kk) = (Xshift' * ...
+            pSigma(:, :, kk) = (Xshift' * ... % sigma
                 (diag(pGamma(:, kk)) * Xshift)) / Nk(kk);
         end
  
@@ -74,30 +83,41 @@ function varargout = gmm(X, K_or_centroids)
     end
  
     function [pMiu pPi pSigma] = init_params()
+		% initialize mu, sigma and pi
+
         pMiu = centroids;
-        pPi = zeros(1, K);
+		% pSigma and pPi need further computation.
         pSigma = zeros(D, D, K);
+        pPi = zeros(1, K);
  
         % hard assign x to each centroids
+
+		% Compute the distance of each sample to each centroid, that is, to compute x^2 + mu^2 - 2mu*x. It will help compute pi and sigma.
         distmat = repmat(sum(X.*X, 2), 1, K) + ...
             repmat(sum(pMiu.*pMiu, 2)', N, 1) - ...
             2*X*pMiu';
         [dummy labels] = min(distmat, [], 2);
  
+		% Compute pi and sigma.
         for k=1:K
             Xk = X(labels == k, :);
+
             pPi(k) = size(Xk, 1)/N;
             pSigma(:, :, k) = cov(Xk);
         end
     end
  
     function Px = calc_prob()
+		% Compute the probablity of normal distribution.
         Px = zeros(N, K);
         for k = 1:K
+
             Xshift = X-repmat(pMiu(k, :), N, 1);
             inv_pSigma = inv(pSigma(:, :, k));
-            tmp = sum((Xshift*inv_pSigma) .* Xshift, 2);
+
             coef = (2*pi)^(-D/2) * sqrt(det(inv_pSigma));
+            tmp = sum((Xshift*inv_pSigma) .* Xshift, 2);
+
             Px(:, k) = coef * exp(-0.5*tmp);
         end
     end
